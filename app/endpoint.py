ba25025aa11f72837b1de20e93f5e3dda65234d3
bartholomew-a-m365group <bartholomew.a@m365connect.com>
2025-03-26 18:39:11 +0100

    Configured webhook

--- app/endpoint.py
+++ app/endpoint.py
@@ -31,6 +31,7 @@ async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     try:
         while True:
+            
             data = await websocket.receive_text()
             # Echo back and broadcast
             await websocket.send_text(f"Message received: {data}")
